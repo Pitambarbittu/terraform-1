a2584db4efd112437aa0b22c9adefb9050f9edee
Pitambarbittu <99995315+Pitambarbittu@users.noreply.github.com>
2023-01-20 18:30:22 +0530

    terraform first day

---
+++ new1.tf
@@ -0,0 +1,41 @@
+resource "local_file" "bittu" {
+  content  = "hey pitambar "
+  filename = "f1.text"
+
+}
+
+resource "local_file" "pitambar" {
+  content  = "hey bittu "
+  filename = "f2.text"
+
+}
+
+resource "local_file" "atul" {
+  content  = "hey atul "
+  filename = "f3.text"
+
+}
+
+resource "local_file" "pushpendra" {
+  content  = "hey pushpa "
+  filename = "f4.text"
+
+}
+
+resource "local_file" "mokshit" {
+  content  = "hey mokshit "
+  filename = "f5.text"
+
+}
+
+resource "local_file" "rahul" {
+  content  = "hey rahul "
+  filename = "f6.text"
+
+}
+
+resource "local_file" "pankaj" {
+  content  = "hey pankaj "
+  filename = "f7.text"
+
+}
